4cf0cde1abccf08350d3ecdc9cc8ff82230799aa
posite <su84053@gmail.com>
2024-01-13 15:57:20 +0900

    feat: 마이페이지 이미지 및 프로필부분 화면 구성

---
+++ app/src/main/java/com/umc/ttoklip/presentation/mypage/MyPageFragment.kt
@@ -0,0 +1,16 @@
+package com.umc.ttoklip.presentation.mypage
+
+import android.os.Bundle
+import android.view.View
+import com.umc.ttoklip.R
+import com.umc.ttoklip.databinding.FragmentMyPageBinding
+import com.umc.ttoklip.presentation.base.BaseFragment
+
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
