9b8113c6cbf10928d495f77c8e6e589f414e8959
hangunhee39 <ghee0831@naver.com>
2024-01-14 21:21:18 +0900

    feat : new UI 구현

---
+++ app/src/main/java/com/umc/ttoklip/presentation/news/NewsViewModel.kt
@@ -0,0 +1,4 @@
+package com.umc.ttoklip.presentation.news
+
+interface NewsViewModel {
+}
---
+++ app/src/main/java/com/umc/ttoklip/presentation/news/detail/NewsDetailViewModelImpl.kt
@@ -0,0 +1,12 @@
+package com.umc.ttoklip.presentation.news.detail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsDetailViewModelImpl @Inject constructor(
+
+): ViewModel(), NewsDetailViewModel {
+
+}
