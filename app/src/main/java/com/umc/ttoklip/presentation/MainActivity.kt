5db8269ff59cde5e71bde405d1c321b490ca0ead
hangunhee39 <ghee0831@naver.com>
2024-01-18 23:09:31 +0900

    feat : 바텀네비게이션 설정

--- app/src/main/java/com/umc/ttoklip/presentation/MainActivity.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/MainActivity.kt
@@ -2,11 +2,27 @@ package com.umc.ttoklip.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.umc.ttoklip.R
+import com.umc.ttoklip.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
