78507e77de0d1201883254b48b12cc978f3a6647
hangunhee39 <ghee0831@naver.com>
2024-01-18 23:11:26 +0900

    feat : 홈 데이터 바인딩 작업

--- app/src/main/java/com/umc/ttoklip/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/home/HomeFragment.kt
@@ -1,18 +1,40 @@
 package com.umc.ttoklip.presentation.home
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.umc.ttoklip.R
 import com.umc.ttoklip.databinding.FragmentHomeBinding
 import com.umc.ttoklip.presentation.base.BaseFragment
+import com.umc.ttoklip.presentation.news.detail.NewsDetailActivity
+import com.umc.ttoklip.presentation.search.SearchActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    private val viewModel: HomeViewModel by viewModels<HomeViewModelImpl>()
+
     override fun initObserver() {
-        TODO("Not yet implemented")
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.activityBus.collect{
+                    when (it) {
+                        HomeViewModel.ActivityEventBus.SEARCH -> startActivity(SearchActivity.newIntent(requireContext()))
+                        HomeViewModel.ActivityEventBus.NEWS_DETAIL -> startActivity(NewsDetailActivity.newIntent(requireContext()))
+                        HomeViewModel.ActivityEventBus.TIP_DETAIL -> {}
+                        HomeViewModel.ActivityEventBus.ALARM -> {}
+                        else -> {}
+                    }
+                }
+            }
+        }
     }
 
     override fun initView() {
-        TODO("Not yet implemented")
+        binding.vm = viewModel
     }
 
 }
--- app/src/main/java/com/umc/ttoklip/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/home/HomeViewModel.kt
@@ -1,4 +1,24 @@
 package com.umc.ttoklip.presentation.home
 
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
 interface HomeViewModel {
+    val haveWork : StateFlow<Boolean>
+    val doneWork : StateFlow<Boolean>
+    val activityBus : SharedFlow<ActivityEventBus>
+
+    fun clickDelayWork()
+    fun clickDoneWork()
+    fun clickMoreNews()
+    fun clickMoreTip()
+    fun clickAlarm()
+    fun clickSearch()
+
+    enum class ActivityEventBus{
+        SEARCH,
+        NEWS_DETAIL,
+        TIP_DETAIL,
+        ALARM
+    }
 }
