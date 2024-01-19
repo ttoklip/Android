4aee3d30f54d1c28b16e72424a8ba68a41a29c18
posite <su84053@gmail.com>
2024-01-19 23:41:18 +0900

    feat: 내 프로필 관리 UI 구현

--- app/src/main/java/com/umc/ttoklip/presentation/mypage/ManageMyInfoActivity.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/mypage/ManageMyInfoActivity.kt
@@ -1,5 +1,6 @@
 package com.umc.ttoklip.presentation.mypage
 
+import android.view.inputmethod.InputMethodManager
 import com.umc.ttoklip.R
 import com.umc.ttoklip.databinding.ActivityManageMyInfoBinding
 import com.umc.ttoklip.presentation.base.BaseActivity
@@ -8,10 +9,21 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ManageMyInfoActivity :
     BaseActivity<ActivityManageMyInfoBinding>(R.layout.activity_manage_my_info) {
-    override fun onStart() {
-        super.onStart()
+    override fun initView() {
+        initViewListener()
+    }
+
+    private fun initViewListener() {
         binding.manageMyInfoBackBtn.setOnClickListener {
             this@ManageMyInfoActivity.onBackPressedDispatcher.onBackPressed()
         }
+
+        binding.root.setOnClickListener {
+            val imm: InputMethodManager =
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
     }
+
+    override fun initObserver() = Unit
 }
--- app/src/main/java/com/umc/ttoklip/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/mypage/MyPageFragment.kt
@@ -1,8 +1,6 @@
 package com.umc.ttoklip.presentation.mypage
 
 import android.content.Intent
-import android.os.Bundle
-import android.view.View
 import com.umc.ttoklip.R
 import com.umc.ttoklip.databinding.FragmentMyPageBinding
 import com.umc.ttoklip.presentation.base.BaseFragment
@@ -11,8 +9,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun initObserver() = Unit
+
+    override fun initView() {
         binding.updateProfileBtn.setOnClickListener {
             val intent = Intent(requireContext(), ManageMyInfoActivity::class.java)
             startActivity(intent)
