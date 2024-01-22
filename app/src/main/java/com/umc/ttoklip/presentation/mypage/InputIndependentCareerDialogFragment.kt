4cc2b4ac9b2f5b0b21927e18e69fb79fbb5c8daf
posite <su84053@gmail.com>
2024-01-23 00:28:54 +0900

    chore: 상수 분리 및 ui 오류 수정

--- app/src/main/java/com/umc/ttoklip/presentation/mypage/InputIndependentCareerDialogFragment.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/mypage/InputIndependentCareerDialogFragment.kt
@@ -1,6 +1,9 @@
 package com.umc.ttoklip.presentation.mypage
 
+import android.os.Build
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.NumberPicker
+import androidx.annotation.RequiresApi
 import com.umc.ttoklip.R
 import com.umc.ttoklip.databinding.FragmentInputIndependentCareerBinding
 import com.umc.ttoklip.presentation.base.BaseBottomSheetDialogFragment
@@ -11,20 +14,34 @@ class InputIndependentCareerDialogFragment(private val btnClickListener: (Int, I
     BaseBottomSheetDialogFragment<FragmentInputIndependentCareerBinding>(R.layout.fragment_input_independent_career) {
     override fun initObserver() = Unit
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun initView() {
+        this@InputIndependentCareerDialogFragment.dialog?.window?.setLayout(
+            WRAP_CONTENT,
+            WRAP_CONTENT
+        )
         with(binding.yearSelector) {
-            minValue = 0
-            maxValue = 99
+            minValue = PERIOD_START
+            maxValue = YEAR_END
             descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            selectionDividerHeight = DIVIDER_HEIGHT_ZERO
         }
         with(binding.monthSelector) {
-            minValue = 0
-            maxValue = 11
+            minValue = PERIOD_START
+            maxValue = MONTH_END
             descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            selectionDividerHeight = DIVIDER_HEIGHT_ZERO
         }
         binding.chooseBtn.setOnClickListener {
             btnClickListener(binding.yearSelector.value, binding.monthSelector.value)
             this@InputIndependentCareerDialogFragment.dismiss()
         }
+    }
+
+    companion object {
+        private const val PERIOD_START = 0
+        private const val YEAR_END = 99
+        private const val MONTH_END = 12
+        private const val DIVIDER_HEIGHT_ZERO = 0
     }
 }
