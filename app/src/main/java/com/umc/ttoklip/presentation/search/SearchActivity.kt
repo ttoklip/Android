def7effed8095e7fae28d96abdad3aeddbb61753
hangunhee39 <ghee0831@naver.com>
2024-01-23 22:09:46 +0900

    feat: 검색 필터 하는 중

--- app/src/main/java/com/umc/ttoklip/presentation/search/SearchActivity.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/search/SearchActivity.kt
@@ -3,10 +3,14 @@ package com.umc.ttoklip.presentation.search
 import android.content.Context
 import android.content.Intent
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.umc.ttoklip.R
 import com.umc.ttoklip.databinding.ActivitySearchBinding
 import com.umc.ttoklip.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchActivity : BaseActivity<ActivitySearchBinding>(R.layout.activity_search) {
@@ -18,12 +22,27 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(R.layout.activity_sea
         binding.backBtn.setOnClickListener {
             finish()
         }
+        binding.filterBtn.setOnClickListener {
+
+        }
     }
 
     override fun initObserver() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.showDialog.collect {
+                    if (it) {
+                        val sheet = BottomDialogSearchFragment { filter ->
+                            viewModel.filter(filter[0], filter[1], filter[2])
+                        }
+                        sheet.show(supportFragmentManager, sheet.tag)
+                    }
+                }
+            }
+        }
     }
 
-    companion object{
+    companion object {
         const val SEARCH_ACTIVITY = "search"
         fun newIntent(context: Context) = Intent(context, SearchActivity::class.java)
     }
--- app/src/main/java/com/umc/ttoklip/presentation/search/SearchViewModelImpl.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/search/SearchViewModelImpl.kt
@@ -1,9 +1,44 @@
 package com.umc.ttoklip.presentation.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModelImpl @Inject constructor(): ViewModel(), SearchViewModel  {
+
+    private val _showDialog = MutableSharedFlow<Boolean>()
+    private val _filterSort = MutableStateFlow(1)
+    override val filterSort: StateFlow<Int>
+        get() = _filterSort
+    private val _filterBoard = MutableStateFlow(0)
+    override val filterBoard: StateFlow<Int>
+        get() = _filterBoard
+
+    private val _filterCategory  = MutableStateFlow(0)
+    override val filterCategory: StateFlow<Int>
+        get() = _filterCategory
+    override val showDialog: SharedFlow<Boolean>
+        get() = _showDialog
+
+    override fun clickFilter() {
+        viewModelScope.launch {
+            _showDialog.emit(true)
+        }
+    }
+
+    override fun filter(sort: Int, board: Int, category:Int) {
+        viewModelScope.launch {
+            _filterSort.value = sort
+            _filterBoard.value =board
+            _filterCategory.value =category
+        }
+    }
 }
