ca4a62d3829b4cde183b86a0270af5d9963da607
40food <40food@naver.com>
2024-01-30 22:12:05 +0900

    feat: splash, intro, login, signup
    화면 연결 및 자잘한 기능 추가

--- app/src/main/java/com/umc/ttoklip/presentation/intro/SplashActivity.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/intro/SplashActivity.kt
@@ -1,13 +1,27 @@
 package com.umc.ttoklip.presentation.intro
 
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import com.umc.ttoklip.R
 import com.umc.ttoklip.databinding.ActivitySplashBinding
+import com.umc.ttoklip.presentation.MainActivity
 import com.umc.ttoklip.presentation.base.BaseActivity
+import com.umc.ttoklip.presentation.login.LoginActivity
 
 class SplashActivity:BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun initView() {
+        val handler= Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val spf=getSharedPreferences("first", MODE_PRIVATE)
+            val firstRun=spf.getBoolean("firstRun",true)
+            if(firstRun){
+                startActivity(Intent(this, IntroActivity::class.java))
+            }else{
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        },2000)
     }
-
     override fun initObserver() {
     }
 }
