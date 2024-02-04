fb0ddd5a2a061dbfec64cdaf23cbc4283e8a28f2
posite <su84053@gmail.com>
2024-02-04 23:06:21 +0900

    feat: 우리동네 기본 UI 구성

--- app/src/main/java/com/umc/ttoklip/presentation/hometown/MyHometownFragment.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/hometown/MyHometownFragment.kt
@@ -1,16 +1,57 @@
 package com.umc.ttoklip.presentation.hometown
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.ttoklip.R
 import com.umc.ttoklip.databinding.FragmentMyHometownBinding
 import com.umc.ttoklip.presentation.base.BaseFragment
+import com.umc.ttoklip.presentation.hometown.adapter.Together
+import com.umc.ttoklip.presentation.hometown.adapter.TogetherAdapter
 import com.umc.ttoklip.presentation.mypage.SortSpinnerAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MyHometownFragment : BaseFragment<FragmentMyHometownBinding>(R.layout.fragment_my_hometown) {
+    private val togetherAdapter by lazy { TogetherAdapter() }
+    private val communicationAdapter by lazy { TogetherAdapter() }
     override fun initObserver() {
 
     }
 
     override fun initView() {
+        initDropdownSortFilter()
+        initTogetherRv()
+        initCommunicationRv()
+    }
+
+    private fun initCommunicationRv() {
+        val togethers = listOf(
+            Together("같이 햇반 대량 구매하실 분?", "상록구 해양동 한양대학로 12"),
+            Together("같이 햇반 대량 구매하실 분?", "상록구 해양동 한양대학로 12"),
+            Together("같이 햇반 대량 구매하실 분?", "상록구 해양동 한양대학로 12")
+        )
+
+        binding.communicationRv.apply {
+            adapter = communicationAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        communicationAdapter.submitList(togethers)
+    }
+
+    private fun initTogetherRv() {
+        val togethers = listOf(
+            Together("같이 햇반 대량 구매하실 분?", "상록구 해양동 한양대학로 12"),
+            Together("같이 햇반 대량 구매하실 분?", "상록구 해양동 한양대학로 12"),
+            Together("같이 햇반 대량 구매하실 분?", "상록구 해양동 한양대학로 12")
+        )
+
+        binding.togetherRv.apply {
+            adapter = togetherAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        togetherAdapter.submitList(togethers)
+    }
+
+    private fun initDropdownSortFilter() {
         val sortFilters = listOf(
             getString(R.string.sort_most_recent),
             getString(R.string.sort_popularity),
---
+++ app/src/main/java/com/umc/ttoklip/presentation/hometown/adapter/TogetherAdapter.kt
@@ -0,0 +1,57 @@
+package com.umc.ttoklip.presentation.hometown.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.umc.ttoklip.databinding.ItemTogetherBinding
+
+class TogetherAdapter :
+    ListAdapter<Together, TogetherAdapter.TogetherViewHolder>(diff) {
+    inner class TogetherViewHolder(
+        private val binding: ItemTogetherBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data: Together, pos: Int) {
+            binding.togetherTitleTv.text = data.title
+            binding.togetherAddressTv.text = data.address
+            if (pos == itemCount - 1) {
+                binding.divider.isGone = true
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TogetherViewHolder {
+        return TogetherViewHolder(
+            ItemTogetherBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: TogetherViewHolder, position: Int) {
+        holder.bind(getItem(position), position)
+    }
+
+    companion object {
+        val diff = object : DiffUtil.ItemCallback<Together>() {
+            override fun areItemsTheSame(
+                oldItem: Together,
+                newItem: Together
+            ): Boolean {
+                return oldItem.title == newItem.title
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Together,
+                newItem: Together
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
