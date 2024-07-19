f604a0c871d1417287b083b5103d3c12741d9cff
40food <40food@naver.com>
2024-07-19 13:21:53 +0900

    onReceivedSslError 처리 추가

--- app/src/main/java/com/umc/ttoklip/presentation/signup/fragments/WebviewActivity.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/signup/fragments/WebviewActivity.kt
@@ -1,7 +1,9 @@
 package com.umc.ttoklip.presentation.signup.fragments
 
 import android.annotation.SuppressLint
+import android.net.http.SslError
 import android.util.Log
+import android.webkit.SslErrorHandler
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
@@ -20,7 +22,31 @@ class WebviewActivity :BaseActivity<ActivityWebviewBinding>(R.layout.activity_we
         //속도 개선
 
         //웹뷰에서 새 창이 뜨지 않도록 방치
-        binding.webView.webViewClient= WebViewClient()
+        binding.webView.webViewClient= object: WebViewClient(){
+            override fun onReceivedSslError(
+                view: WebView?,
+                handler: SslErrorHandler?,
+                error: SslError?
+            ) {
+                super.onReceivedSslError(view, handler, error)
+                handler?.proceed()
+                val builder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(applicationContext)
+                var message = "SSL Certificate error"
+                when (error?.primaryError) {
+                    SslError.SSL_UNTRUSTED -> message = "신뢰할 수 없는 사이트입니다."
+                    SslError.SSL_EXPIRED -> message = "만료된 사이트입니다."
+                    SslError.SSL_IDMISMATCH -> message = "도메인이 없습니다."
+                    SslError.SSL_NOTYETVALID -> message = "검증되지 않은 사이트입니다."
+                }
+                message += "페이지로 이동 하시겠습니까?"
+                builder.setTitle("SSL Certificate Error")
+                builder.setMessage(message)
+                builder.setPositiveButton("확인") { _, _ -> handler?.proceed() }
+                builder.setNegativeButton("취소") { _, _ -> handler?.cancel() }
+                val dialog: android.app.AlertDialog = builder.create()
+                dialog.show()
+            }
+        }
         binding.webView.webChromeClient= WebChromeClient()
 
         //링크 주소 로드
