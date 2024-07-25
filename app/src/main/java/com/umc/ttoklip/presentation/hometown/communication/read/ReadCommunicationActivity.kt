5245142062eaa7ea192116f98d53ee7da481d0e5
KKJ <kyujin0911@tukorea.ac.kr>
2024-07-26 00:15:52 +0900

    fix: 소통해요 글 작성 로직 수정

--- app/src/main/java/com/umc/ttoklip/presentation/hometown/communication/read/ReadCommunicationActivity.kt
+++ app/src/main/java/com/umc/ttoklip/presentation/hometown/communication/read/ReadCommunicationActivity.kt
@@ -259,13 +259,10 @@ class ReadCommunicationActivity :
         return super.dispatchTouchEvent(event)
     }
 
-    override fun onClick(imageUrl: String) {
-        val images = imageAdapter.currentList.filterIsInstance<ImageUrl>().map { it.imageUrl }
+    override fun onClick(imageUrl: String, position: Int) {
+        val images = imageAdapter.currentList.filterIsInstance<com.umc.ttoklip.data.model.town.ImageUrl>().map { it.communityImageUrl }
             .toTypedArray()
-        Log.d("images", images.toString())
-        val intent = Intent(this, ReadImageViewActivity::class.java)
-        intent.putExtra("images", images)
-        startActivity(intent)
+        startActivity(ReadImageViewActivity.newIntent(this, images, position))
     }
 
     companion object {
